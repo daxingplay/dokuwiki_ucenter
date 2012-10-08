405c99c719230050d69787778d970453dd61d1c2
daxingplay <daxingplay@gmail.com>
2012-10-08 13:23:07 +0800

    change default group type to empty

---
+++ UnitTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @fileoverview
+ * @author ×ÏÓ¢£¨daxingplay£©<daxingplay@gmail.com>
+ * @time: 12-10-7 23:49
+ * @description
+ */
+class DokuwikiUCAuthTest extends PHPUnit_Framework_TestCase{
+    public function testLoad(){
+        $this->assertEquals(0, 0);
+    }
+}
